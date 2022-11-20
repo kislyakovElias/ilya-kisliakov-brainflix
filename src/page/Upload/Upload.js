fd4e45f134c998e6f1f63459ce78a697706ed19d
super_fox <super_fox@mail.ru>
2022-11-20 15:19:49 -0800

    upload fixed

--- src/page/Upload/Upload.js
+++ src/page/Upload/Upload.js
@@ -2,7 +2,6 @@ import "./Upload.scss";
 import Header from "../../components/Header/Header";
 import uploadIcon from "../../assets/Icons/publish.svg";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
 import axios from "axios";
 
 function Upload() {
